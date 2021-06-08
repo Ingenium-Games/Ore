0be314c02684e084b393b34f2a8ad7d948047412
Twiitchter <andylutze6090@gmail.com>
2021-06-08 18:58:22 +1000

    Toned down the markers. Added Text for help hints

--- client/_markers.lua
+++ client/_markers.lua
@@ -7,11 +7,10 @@ NOTES.
     -
     -
     -
-]] --
+]]--
 math.randomseed(c.Seed)
 -- ====================================================================================--
 -- https://docs.fivem.net/docs/game-references/markers/
-
 
 --- Select a premade marker style.
 ---@param v number "A number to select corresponding local array value."
@@ -25,29 +24,36 @@ function c.markers.SelectMarker(v, ords)
         end,
         [1] = function()
             -- Blue Static $.
-            DrawMarker(29, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 0, 55, 240, 35, 0, 0, 2, 0)
+            DrawMarker(29, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 0, 55, 240, 35, 0, 0, 2,
+                0)
         end,
         [2] = function()
             -- Blue Static ?.
-            DrawMarker(32, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 0, 55, 240, 35, 0, 0, 2, 0)
+            DrawMarker(32, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 0, 55, 240, 35, 0, 0, 2,
+                0)
         end,
         [3] = function()
             -- Blue Static Chevron.
-            DrawMarker(20, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 0, 55, 240, 35, 0, 0, 2, 0)
+            DrawMarker(20, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 0, 55, 240, 35, 0, 0, 2,
+                0)
         end,
         [4] = function() -- Mainly for pickups or notes or anything found on ground.
             -- Small White Rotating Circle + Bouncing ? (on Ground)
-            DrawMarker(27, ords[1], ords[2], ords[3]-0.45, 0, 0, 0, 0, 0, 0, 0.4001, 0.4001, 0.4001, 240, 240, 240, 35, 0, 0, 2, 1)
-            DrawMarker(32, ords[1], ords[2], ords[3]-0.45, 0, 0, 0, 0, 0, 0, 0.2001, 0.4001, 0.8001, 240, 240, 240, 35, 1, 1, 2, 0)
+            DrawMarker(27, ords[1], ords[2], ords[3] - 0.45, 0, 0, 0, 0, 0, 0, 0.4001, 0.4001, 0.4001, 240, 240, 240,
+                35, 0, 0, 2, 1)
+            DrawMarker(32, ords[1], ords[2], ords[3] - 0.45, 0, 0, 0, 0, 0, 0, 0.2001, 0.4001, 0.8001, 240, 240, 240,
+                35, 1, 1, 2, 0)
         end,
         [5] = function()
             -- White Rotating Chevron Bouncing.
-            DrawMarker(29, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 240, 240, 240, 35, 1, 0, 2, 1)
+            DrawMarker(29, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 0.7001, 1.0001, 0.3001, 240, 240, 240, 35, 1, 0,
+                2, 1)
         end,
         [6] = function()
             -- Blue Static $.
-            DrawMarker(29, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 1.7001, 0, 55, 240, 35, 0, 0, 2, 0)
-        end,
+            DrawMarker(29, ords[1], ords[2], ords[3], 0, 0, 0, 0, 0, 0, 1.001, 1.0001, 1.7001, 0, 55, 240, 35, 0, 0, 2,
+                0)
+        end
     }
     if num then
         return markers[num]
@@ -55,149 +61,44 @@ function c.markers.SelectMarker(v, ords)
 end
 
 --- Produce A loop of markers to generate based on criteria.
----@param t table "Contains {["coords"] = vector3(), ["number"] = 0,X}"
----@param t2 table "Contains two values, first should be job, item or vehicle. Second is a Callback. ie {["vehicle"] = class, ["func"] = cb()}"
-function c.markers.CreateThreadLoop(t, t2)
+---@param t table 'Contains coords as vector3, number for marker selection with c.marker functions, notification for dynamic entry with c.text functions and a func for callback function to do. {["coords"] = vector3(), ["number"] = 0,X, ["notification"] = {"KEYBOARD_USE", "DO X Y Z"}, ["callback"] = cb()}'
+function c.markers.CreateThreadLoop(t)
     local tab = c.check.Table(t)
     local size = c.table.SizeOf(tab)
-    if not t2 then
-        -- Create the loop based on the Coordinates and marker style provided.
-        Citizen.CreateThread(function()
-            local tab = tab
-            local size = size
-            while true do
-                local found = false
-                local close = true
-                local ped = PlayerPedId()
-                local pos = vector3(GetEntityCoords(ped))
-                Citizen.Wait(1)    
-                if c.data.GetLoadedStatus() then        
-                    for i=1, size, 1 do
-                        local ords = tab[i].coords
-                        local num = tab[i].number
-                        if (Vdist(pos, ords) < 20) then
-                            found = true
-                            -- Draw marker
-                            c.markers.SelectMarker(num, ords)
-                            if (Vdist(pos, ords) < 5) then
-                                close = true
-                                -- Help notifications??
-
+    -- Create the loop based on the Coordinates and marker style provided.
+    Citizen.CreateThread(function()
+        local tab = tab
+        local size = size
+        while true do
+            local found = false
+            local close = false
+            local ped = PlayerPedId()
+            local pos = vector3(GetEntityCoords(ped))
+            Citizen.Wait(1)
+            if c.data.GetLoadedStatus() then
+                for i = 1, size, 1 do
+                    local ords = tab[i].coords
+                    local num = tab[i].number
+                    local text = tab[i].notification
+                    local cb = tab[i].callback
+                    if (Vdist(pos, ords) < 20) then
+                        found = true
+                        -- Draw marker
+                        c.markers.SelectMarker(num, ords)
+                        if (Vdist(pos, ords) < 5) then
+                            close = true
+                            -- Show help
+                            c.text.DisplayHelp(text[1], text[2])
+                            if IsControlJustPressed(0, 38) then
+                                -- Do action.
+                                cb()
                             end
                         end
                     end
-                else
-                    Citizen.Wait(1250)
                 end
+            else
+                Citizen.Wait(1250)
             end
-        end)
-    elseif t2["job"] then
-        -- Create the loop based on the Coordinates and marker style provided.
-        Citizen.CreateThread(function()
-            local tab = tab
-            local size = size
-            while true do
-                local found = false
-                local close = true
-                local ped = PlayerPedId()
-                local pos = vector3(GetEntityCoords(ped))
-                local job = c.data.GetPlayer().Job.Name
-                Citizen.Wait(1)    
-                if c.data.GetLoadedStatus() then
-                    if job == t2["job"] then
-                        for i=1, size, 1 do
-                            local ords = tab[i].coords
-                            local num = tab[i].number
-                            if (Vdist(pos, ords) < 20) then
-                                found = true
-                                -- Draw marker
-                                c.markers.SelectMarker(num, ords)
-                                if (Vdist(pos, ords) < 5) then
-                                    close = true
-                                    -- Help notifications??
-                                    if IsControlJustPressed(0, 54) then --E
-                                        t2["func"]()
-                                    end
-                                end
-                            end
-                        end
-                    end
-                else
-                    Citizen.Wait(1250)
-                end
-            end
-        end)
-    elseif t2["item"] then
-        -- Create the loop based on the Coordinates and marker style provided.
-        Citizen.CreateThread(function()
-            local tab = tab
-            local size = size
-            while true do
-                local found = false
-                local close = true
-                local ped = PlayerPedId()
-                local pos = vector3(GetEntityCoords(ped))
-                local item = exports["inventory"].HasItem(t2["item"])
-                Citizen.Wait(1)    
-                if c.data.GetLoadedStatus() then        
-                    if item then
-                        for i=1, size, 1 do
-                            local ords = tab[i].coords
-                            local num = tab[i].number
-                            if (Vdist(pos, ords) < 20) then
-                                found = true
-                                -- Draw marker
-                                c.markers.SelectMarker(num, ords)
-                                if (Vdist(pos, ords) < 5) then
-                                    close = true
-                                    -- Help notifications??
-                                    if IsControlJustPressed(0, 54) then --E
-                                        t2["func"]()
-                                    end
-                                end
-                            end
-                        end
-                    end
-                else
-                    Citizen.Wait(1250)
-                end
-            end
-        end)
-    elseif t2["vehicle"] then
-        -- Create the loop based on the Coordinates and marker style provided.
-        Citizen.CreateThread(function()
-            local tab = tab
-            local size = size
-            while true do
-                local found = false
-                local close = true
-                local ped = PlayerPedId()
-                local pos = vector3(GetEntityCoords(ped))
-                local vehclass = GetVehicleClass(GetVehiclePedIsIn(ped))
-                Citizen.Wait(1)    
-                if c.data.GetLoadedStatus() then        
-                    if vehclass == t2["vehicle"] then
-                        for i=1, size, 1 do
-                            local ords = tab[i].coords
-                            local num = tab[i].number
-                            if (Vdist(pos, ords) < 35) then
-                                found = true
-                                -- Draw marker
-                                c.markers.SelectMarker(num, ords)
-                                if (Vdist(pos, ords) < 5) then
-                                    close = true
-                                    -- Help notifications??
-                                    if IsControlJustPressed(0, 54) then --E
-                                        t2["func"]()
-                                    end
-                                end
-                            end
-                        end
-                    end
-                else
-                    Citizen.Wait(1250)
-                end
-            end
-        end)
-    end
+        end
+    end)
 end
---
+++ client/_text.lua
@@ -0,0 +1,34 @@
+-- ====================================================================================--
+--  MIT License 2020 : Twiitchter
+-- ====================================================================================--
+c.text = {}
+c.textentries = {}
+--[[
+NOTES.
+    -
+    -
+    -
+]] --
+math.randomseed(c.Seed)
+-- ====================================================================================--
+
+function c.text.AddEntry(s1, s2)
+    local str1 = string.upper(s1)
+    local str2 = s2
+    if not c.textentries[str1] then
+        c.textentries[str1] = str2
+        AddTextEntry(str1,str2)
+    else
+        c.debug("Text Entry: "..str1.." has already been used. Ignoring this input.")
+    end    
+end
+
+
+function c.text.DisplayHelp(s, addon)
+    SetTextComponentFormat(s)
+    AddTextComponentSubstringPlayerName(addon)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+-- Example to be used in _markers.lua
+c.text.AddEntry("KEYBOARD_USE", "Press ~INPUT_PICKUP~ to ~a~")
