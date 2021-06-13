90000467a63bffa66ca76534c127bea0e1439869
Twiitchter <andylutze6090@gmail.com>
2021-06-13 19:04:00 +1000

    looking to meta_libs to figure out wtf ive done...

--- client/_markers.lua
+++ client/_markers.lua
@@ -2,6 +2,7 @@
 --  MIT License 2020 : Twiitchter
 -- ====================================================================================--
 c.marker = {}
+c.markers = {}
 --[[
 NOTES.
     -
@@ -11,6 +12,130 @@ NOTES.
 math.randomseed(c.Seed)
 -- ====================================================================================--
 -- https://docs.fivem.net/docs/game-references/markers/
+
+Marker = function(type,posX,posY,posZ,dirX,dirY,dirZ,rotX,rotY,rotZ,scaleX,scaleY,scaleZ,red,green,blue,alpha,bobUpAndDown,faceCamera,p19,rotate,textureDict,textureName,drawOnEnts,textDist,text,doFunc,onKey,funcArgs)
+    return {
+      type = (type or 0),
+      posX = (posX or 0.0),
+      posY = (posY or 0.0),
+      posZ = (posZ or 0.0),
+      dirX = (dirX or 0.0),
+      dirY = (dirY or 0.0),
+      dirZ = (dirZ or 0.0),
+      rotX = (rotX or 0.0),
+      rotY = (rotY or 0.0),
+      rotZ = (rotZ or 0.0),
+      scaleX = (scaleX or 0.0),
+      scaleY = (scaleY or 0.0),
+      scaleZ = (scaleZ or 0.0),
+      red = (red or 255),
+      green = (green or 255),
+      blue = (blue or 255),
+      alpha = (alpha or 255),
+      bobUpAndDown = (bobUpAndDown or false),
+      faceCamera = (faceCamera or true),
+      p19 = (p19 or false),
+      rotate = (rotate or true),
+      textureDict = (textureDict or nil),
+      textureName = (textureName or nil),
+      drawOnEnts = (drawOnEnts or nil),
+      pos = vector3((posX or 0.0),(posY or 0.0),(posZ or 0.0)),
+      textDist = (textDist or false),
+      text = (text or false),
+      doFunc = (doFunc or false),
+      onKey = (onKey or false),
+      funcArgs = (funcArgs or false),
+    }
+  end
+
+  local markers = {}
+local chunk = {}
+
+local lastChunk = false
+local chunkDist = 100.0
+local drawDist  =  50.0
+
+local PlayerPos = function()
+  return GetEntityCoords(GetPlayerPed(-1))
+end
+
+local ReChunk = function()
+  local newChunk = {}
+  local plyPos = PlayerPos()
+  for k,v in pairs(markers) do
+    local dist = Vector.Dist(plyPos,v.pos)
+    if dist < chunkDist then
+      newChunk[#newChunk+1] = v
+    end
+  end
+  return newChunk
+end
+
+local RenderMarker = function(marker)
+  DrawMarker(marker.type, marker.posX, marker.posY, marker.posZ, marker.dirX, marker.dirY, marker.dirZ, marker.rotX, marker.rotY, marker.rotZ, marker.scaleX, marker.scaleY, marker.scaleZ, marker.red, marker.green, marker.blue, marker.alpha, marker.bobUpAndDown, marker.faceCamera, marker.p19, marker.rotate, marker.textureDict, marker.textureName, marker.drawOnEnts);
+end
+
+local DrawMarkers = function()
+  local plyPos = PlayerPos()
+  for k,v in pairs(chunk) do
+    local dist = Vector.Dist(plyPos,v.pos)
+    if dist < drawDist then
+      RenderMarker(v)
+      if v.textDist and v.text then
+        if dist < v.textDist then
+          HelpNotification(v.text)
+          if v.doFunc and v.onKey then
+            if IsControlJustReleased(0,v.onKey) or IsDisabledControlJustReleased(0,v.onKey) then
+              Wait(0)
+              v.doFunc(v.funcArgs)
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+Citizen.CreateThread(function()
+  while true do
+    local timeNow = GetGameTimer()
+    local timer = math.ceil(math.max(1,math.min(10,#markers))*100)
+    if (not lastChunk or (timeNow - lastChunk > timer)) then
+      lastChunk = timeNow
+      chunk = ReChunk()
+    end
+    DrawMarkers()
+    Wait(0)
+  end
+end)
+
+exports('AddMarker',function(...)
+  local marker = Marker(...)
+  local handle = #markers+1
+  markers[handle] = marker
+  return handle
+end)
+
+exports('RemoveMarker',function(handle)
+  local marker = markers[handle]
+  if marker then
+    for k,v in pairs(chunk) do
+      if v == marker then
+        chunk[k] = nil
+      end
+    end
+    markers[handle] = nil
+  end
+end)
+
+exports('TeleportToMarker', function(handle)
+  local marker = markers[handle]
+  if marker then
+    TeleportPlayer(marker.pos)
+  end
+end)
+
+
 
 --- Select a premade marker style.
 ---@param v number "A number to select corresponding local array value."
