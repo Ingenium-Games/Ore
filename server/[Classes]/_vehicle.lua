64f68e3565ac6ba8b32918e2ea1e4e52e1e68f9a
Twiitchter <andylutze6090@gmail.com>
2021-06-15 22:12:13 +1000

    Starting Vehicle class and state awareness

--- server/[Classes]/_vehicle.lua
+++ server/[Classes]/_vehicle.lua
@@ -13,36 +13,106 @@ math.randomseed(c.Seed)
 
 -- Too Add vehicle driving fuel consumption.
 
-function c.class.VehicleClass(entity)
-    local _min = 0
-    local _max = 100
+--[[
+
+
+	`ID`
+	`Character_ID`
+	`Model` Hash
+	`Plate` 8 Varchar
+	`Position` '{"x":0.00,"y":0.00,"z":0.00,"h":0.00}'
+	`Keys` json{} Character_ID's
+	`Condition` vfuel, hp, other values
+    `Inventory` json{}
+    `Modifications` json{}handling info, mos and liverys
+	`Garage` 2 character - A1 A2 A3
+	`State` T/F In OUt
+	`Impound` T/F In OUt
+	`Wanted` T/F In OUt
+
+
+]]--
+
+function c.class.VehicleClass(networkid, plate)
+    local data = c.sql.GetVehicleByPlate(plate)
     local self = {}
+    self.Entity = NetworkGetEntityFromNetworkId(networkid)
     --
-    self.Fuel = math.random(73, 100)
+    EnsureEntityStateBag(self.Entity)
+    --
+    self.SetState = function(k,v)
+        Entity(self.Entity).state[k] = v
+    end
+    --
+    self.GetState = function(k)
+        return Entity(self.Entity).state[k]
+    end
+    --
+    self.Model = data.Model
+    self.Plate = data.Plate
+    self.SetState(Plate, self.Plate)
+    self.Coords = json.decode(data.Position)
+    self.SetState(Coords, self.Coords)
+    self.Keys = json.decode(data.Keys)
+    self.SetState(Keys, self.Keys)
+    self.Condition = json.decode(data.Condition)
+    self.Inventory = json.decode(data.Inventory)
+    self.Modifications = json.decode(data.Modifications)
+    self.Instance = data.Instance
+    self.Garage = data.Garage
+    self.State = data.State
+    self.Impound = data.Impound
+    self.Wanted = data.Wanted
+    --
+    self.GetPosition = function()
+        local x,y,z = GetEntityCoords(self.Entity)
+        local h = GetEntityHeading(self.Entity)
+        return x,y,z,h
+    end
+    --
+    self.SetPosition = function()
+        local x,y,z,h = self.GetPosition()
+        self.Coords = {
+            ['x'] = c.math.Decimals(x,2),
+            ['y'] = c.math.Decimals(y,2),
+            ['z'] = c.math.Decimals(z,2),
+            ['h'] = c.math.Decimals(h,2)
+        }
+        self.SetState(Coords, self.Coords)
+        return self.Coords
+    end
+    --
+    self.Fuel = self.Condition.Fuel
+    self.SetState(Fuel, self.Condition.Fuel)
     --
     self.GetFuel = function()
-        return self.Fuel
+        return self.Fuel or self.GetState(Fuel)
     end
     --
     self.AddFuel = function(num)
-        local num = c.check.Number(num, _min, _max)
+        local num = c.check.Number(num, 0, 100)
         self.Fuel = (self.Fuel + num)
+        self.SetState(Fuel, num)
         if self.Fuel >= 100 then
            self.Fuel = 100
+           self.SetState(Fuel, 100)
         end
     end
     --
     self.RemoveFuel = function(num)
-        local num = c.check.Number(num, _min, _max)
+        local num = c.check.Number(num, 0, 100)
         self.Fuel = (self.Fuel - num)
+        self.SetState(Fuel, num)
         if self.Fuel <= 0 then
            self.Fuel = 0
+           self.SetState(Fuel, 0)
         end
     end
     --
     self.SetFuel = function(num)
-        local num = c.check.Number(num, _min, _max)
+        local num = c.check.Number(num, 0, 100)
         self.Fuel = num
+        self.SetState(Fuel, num)
     end
     --
     return self
---
+++ server/_vehicles.lua
@@ -0,0 +1,48 @@
+--[[
+
+
+	`ID`
+	`Character_ID`
+	`Model` Hash
+	`Plate` 8 Varchar
+	`Position` '{"x":0.00,"y":0.00,"z":0.00,"h":0.00}'
+	`Keys` json{} Character_ID's
+	`Condition` vfuel, hp, other values
+    `Inventory` json{}
+    `Modifications` json{}handling info, mos and liverys
+	`Garage` 2 character - A1 A2 A3
+	`State` T/F In OUt
+	`Impound` T/F In OUt
+	`Wanted` T/F In OUt
+
+
+]]--
+
+c.vehicle = {} -- function level
+c.vehicles = {} -- data to save
+
+function c.vehicle.GetVehicle(plate)
+    return c.vehicles[plate]
+end
+
+--- Same as above.
+---@param plate string
+function c.GetVehicle(plate)
+    return c.vehicle.GetVehicle(plate)
+end
+
+function c.vehicle.SetVehicle(plate, data)
+    c.vehicles[plate] = data
+end
+
+function c.vehicle.RemoveVehicle(plate)
+    c.vehicles[plate] = false
+end
+
+function c.vehicle.GetVehicles()
+    return c.vehicles
+end
+
+function c.GetVehicles()
+    return c.vehicle.GetVehicles()
+end
