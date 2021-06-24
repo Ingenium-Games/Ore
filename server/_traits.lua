69e90b1934d5f624ae560e01cc67b26ba53c69c8
Twiitchter <andylutze6090@gmail.com>
2021-06-24 21:38:29 +1000

    Cleanup and thoughts on traits.

--- server/_traits.lua
+++ server/_traits.lua
@@ -11,3 +11,33 @@ NOTES.
 ]]--
 math.randomseed(c.Seed)
 -- ====================================================================================--
+
+--[[
+
+Reviewing implimentations on how traits could work.
+
+EG - Fat / Obease trait.
+    Hunger Increases 2x quicker.
+    Max hp is buffed by 120
+    Movement speed is 0.82
+    Melee attack damage halfed
+    Health slowly decreases while out of stam and running.
+    Eating food items gains regenerates 5 hp instantly, 30 over a mintute. 5 per 10 sec tick.
+
+
+EG - Legs of Steel
+    Cycle speed increased by 20%
+    can run for longer without loosing stam.
+    speed increased to 1.09
+    thirst decreases 1.55x rate.
+    stress increases while not moving.
+    stress decreases while running for longer than 5 minutes.
+    drugs matabolise faster and effects are halved.
+
+
+How to impliment would be another question...
+
+Upon creation or by performaing actions that are tracked via the DB?
+
+]]--
+
--- server/_vehicles.lua
+++ server/_vehicles.lua
@@ -1,24 +1,13 @@
---[[
-
-
-	`ID`
-	`Character_ID`
-	`Model` Hash
-	`Plate` 8 Varchar
-	`Position` '{"x":0.00,"y":0.00,"z":0.00,"h":0.00}'
-	`Keys` json{} Character_ID's
-	`Condition` vfuel, hp, other values
-    `Inventory` json{}
-    `Modifications` json{}handling info, mos and liverys
-	`Garage` 2 character - A1 A2 A3
-	`State` T/F In OUt
-	`Impound` T/F In OUt
-	`Wanted` T/F In OUt
-
-
-]]--
-
+-- ====================================================================================--
+--  MIT License 2020 : Twiitchter
+-- ====================================================================================--
 c.vehicle = {} -- function level
-c.vehicles = {} -- data to save
-
-
+c.vehicles = {} -- xVehicles Table / vdex	
+--[[
+NOTES.
+    -
+    -
+    -
+]]--
+math.randomseed(c.Seed)
+-- ====================================================================================--
